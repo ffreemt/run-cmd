5f198445c6a89593894b7f99095f671ea19eeab5
ffreemt <yucongo+fmt@gmail.com>
2023-10-20 12:31:27 +0800

    Update mute_stdout mute_stderr

--- python_run_cmd/run_cmd.py
+++ python_run_cmd/run_cmd.py
@@ -5,12 +5,25 @@ import rich
 from loguru import logger
 
 
-def run_cmd(cmd):
-    """Execute cmd."""
+def run_cmd(cmd, mute_stdout=True, mute_stderr=False):
+    """
+    Execute cmd.
+
+    Args:
+    ----
+        cmd: command to execute
+        mute_stdout: default True, do not output stdout if True
+        mute_stderr: default False, do not output stderr if True
+
+    Returns:
+    -------
+        CompletedProcess(args=cmd, returncode=0|..., stdout='...', stderr='...'
+    """
     logger.info(f"{cmd=}")
-    ret = sp.run(cmd, capture_output=1, check=0, shell=1, encoding="utf8")
-    if ret.stdout:
+    ret = sp.run(cmd, capture_output=True, check=False, shell=True, encoding="utf8")
+    if ret.stdout and not mute_stdout:
         rich.print(ret.stdout)
-    if ret.stderr:
+    if ret.stderr and not mute_stderr:
         rich.print("[red bold]" + ret.stderr)
+
     return ret
